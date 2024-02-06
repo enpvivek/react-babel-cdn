f9f56199a1133f84aee57405d6c42c01645651f1
enpvivek <enpvivek@gmail.com>
2024-02-06 11:26:45 +0530

    fix: create elements both ways

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 // function to create Button in React
 function MyButton() {
-  return <button>I'm a button</button>;
+  return <button>Click Me</button>;
 }
 
-// Creating H1 with button
+// Creating H1 with button using React Babel
 function MyApp() {
   return (
-    <div>
+    <div id="app">
       <h1>Welcome to my app</h1>
       <MyButton />
+      {parent}
     </div>
   );
 }
+
+// Creating H1 under div using React.createElement
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement(
+      "h1",
+      { id: "heading" },
+      "This is heading under child div which is under parent div"
+    )
+  )
+);
 // binding root div to react and rendering
 ReactDOM.render(<MyApp />, document.getElementById("root"));
